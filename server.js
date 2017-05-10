c8d6ab094b914a5c1247f6010304f5e00c35ae6c
PaulDS <paul.david.sivelle@gmail.com>
2017-05-10 11:30:36 -0700

    Fix some bugs Add logs

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+var exec = require('child_process').exec;
+
 var app = express();
 
 console.log("Starting server...");
@@ -11,14 +14,29 @@ catch(e) {
   process.exit();
 }
 
+app.use(bodyParser.json());
 
-app.get('event', (req, res) => {
-  var specificConf = config.hooks.find(hook => (!hook.event || hook.event == req.object_kind) &&  (!hook.repository || hook.repository == req.repository.name));
+app.post('/event', (req, res) => {
+  console.log(req.body);
+  if (!req.body || !req.body.object_kind || !req.body.repository || !req.body.repository.name) {
+    return res.sendStatus(400);
+  }
+
+  console.log('Receive event ' + req.body.object_kind + ' for repository ' + req.body.repository.name);
+
+  var specificConf = config.hooks.find(hook => (!hook.event || hook.event == req.body.object_kind) &&  (!hook.repository || hook.repository == req.body.repository.name));
 
   if (specificConf) {
+    console.log('Executing ' + specificConf.script + '...');
     exec(specificConf.script, (err,stdout,stderr) => {
       console.log(specificConf.script + ' executed');
     });
+
+    res.sendStatus(200);
+  }
+  else {
+    console.log('No hook found for this event');
+    res.sendStatus(404);
   }
 });
 
